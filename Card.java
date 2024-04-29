9a5d3903abec9a22dd0b83cb2c3f769473e06bc1
beautysan <168405245+beautysan@users.noreply.github.com>
2024-04-29 11:35:48 +0000

    created static variables

--- Card.java
+++ Card.java
@@ -1,5 +1,11 @@
 public abstract class Card {
+    public static final int ROGUE = 0; 
+    public static final int BARBARIAN = 1; 
+    public static final int WIZARD = 2; 
+    public static final int PALADIN = 3;
+    
     private int hp; 
+    
 
     public Card(int hp){
         this.hp = hp; 
