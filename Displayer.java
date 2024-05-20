03585f916843ca7ad723f67c5d3a76aabac2e341
amarazin <68317938+amarazin@users.noreply.github.com>
2024-05-20 06:11:45 +0000

    comments in displayer

--- Displayer.java
+++ Displayer.java
@@ -2,10 +2,10 @@ public class Displayer {
     public Displayer(){
     }
 
-     //==================================PRIVATE METHOD====================================//
+    //==================================PRIVATE METHOD====================================//
 
     /*
-        - rowNum(len: int): int
+        
         - playerType(p: Player): String
         - cardType(card: Card): String
         - getStatus(p: Player): String
@@ -13,6 +13,10 @@ public class Displayer {
     */
 
     // DEVELOPED BY: AMARA
+    /* Recursive method to determine the n number of rows needed if cards are to be sorted in a 4 x n grid. 
+     * @param len - number of cards that still needs to be sorted
+     * @return    - number of rows (n) needed 
+     */
     private int rowNum(int len){
         if(len == 4 || len/4 == 0){
             return 1; 
@@ -59,36 +63,49 @@ public class Displayer {
     }
 
     // DEVELOPED BY: AMARA
+    /* Output one row of cards in the card hand. Each row can have a maximum of four cards, minimum of 1
+     * @param cards - Card array of card hand 
+     * @param num   - number of cards to display 
+     * @param count - card number of the first card in each row 
+     */
     private void printCards(Card[] cards, int num,int count){
+        // display the top border of cards 
         for(int j = 0; j < num; j++){
             System.out.print("------------------------------     ");
         }
         System.out.println(" ");
+        // display the info of cards 
         for(int j = 0; j < num; j++){
+            // count+j is used to access the exact card in the array
             String cardInfo = cardType(cards[count + j]) +" "+ Integer.toString(count+j+1);
             System.out.printf("|%-28s|     ", cardInfo);
         }
         System.out.println(" ");
+        // ddisplay the attack info of cards
         for(int j = 0; j < num; j++){
             String attack = "attack : "+Integer.toString(cards[count + j].GetAttackCount());
             System.out.printf("|%28s|     ",attack);
         }
         System.out.println(" ");
+        // ddisplay the defese info of cards
         for(int j = 0; j < num; j++){
             String defense = "defense : "+Integer.toString(cards[count + j].GetDefenseCount());
             System.out.printf("|%28s|     ",defense);
         }
         System.out.println(" ");
+        // ddisplay the special info of cards
         for(int j = 0; j < num; j++){
             String special = "special : "+Integer.toString(cards[count + j].GetSpecialCount());
             System.out.printf("|%28s|     ",special);
         }
         System.out.println(" ");
+        // ddisplay the heal info of cards
         for(int j = 0; j < num; j++){
             String heal = "heal : "+Integer.toString(cards[count + j].GetHealCount());
             System.out.printf("|%28s|     ",heal);
         }
         System.out.println(" ");
+        // ddisplay the bottom border of cards
         for(int j = 0; j < num; j++){
             System.out.print("------------------------------     ");
         }
@@ -104,18 +121,29 @@ public class Displayer {
     }
 
     // DEVELOPED BY: AMARA
+    /* Displays all the cards in hand with the format of a 4xn grid 
+     * @param cards - Card[] array of card hand
+     */
     public void ShowCardHand(Card[] cards){
+        // find number of rows 
         int rows = rowNum(cards.length);
+        // keep track of the remaining cards left to display 
         int col = cards.length; 
+        // keep track of the card number required to dispaly 
         int count = 0; 
+
+        // loop the cards depending on the number of rows 
         for(int i=0; i<rows; i++){
+            // if there are more than 4 cards left to display, display a full row of cards 
             if(col >= 4){
                 printCards(cards, 4, count);
                 System.out.println(" ");
             }
+            // if not display the remaining cards left for the last row 
             else{
                 printCards(cards, col, count);
             }
+            // incease the card number by four, and decrase the remaining cards by four to continue on the next four set of cards 
             count += 4; 
             col -=4;
         }
@@ -132,20 +160,28 @@ public class Displayer {
     }
 
     // DEVELOPED BY: AMARA
+    /* Display the HP Scale and the Sheild of each player
+     * @param hp      - player's hp 
+     * @param sheild  - player's number of sheilds 
+     */
     public void DisplayPlayerDetails(int hp, int shield){
         System.out.println("|============== HP SCALE ===============|         |====== SHEILD ===========================|");
         System.out.println("-----------------------------------------         -------------------------------------------");
         // Display HP 
+        // if there is a full hp, display an empty hp scale bar
         if(hp == 10){
             for(int i=0; i<hp; i++){
                 System.out.printf("%-4s","|");
             }
             System.out.print("|");
         }
+        // if not then display how many damages it took 
         else{
+            // display the damages that it took 
             for(int i=0; i<10-hp;i++){
                 System.out.print("| X ");
             }
+            // display the empty grid of remaining hp 
             for(int i=0; i<hp; i++){
                 System.out.printf("%-4s","|");
             }
@@ -154,9 +190,12 @@ public class Displayer {
         System.out.print("         ");
 
         // Display Shield
+        // prompt a message that there are no sheilds if the shield count is zero
         if(shield==0){
             System.out.println("You have no shields at the moment");
         }
+
+        // display the number of sheilds that the player aquired 
         else{
             for(int i=0; i<shield; i++){
                 System.out.print("| S ");
